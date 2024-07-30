410ef2eaff35ef062547b190a4d89303ed64a102
Dhafa11 <dhafasj11@gmail.com>
2024-07-30 09:41:00 +0700

    Add files via upload

---
+++ Pertemuan1/perkenalan.py
@@ -0,0 +1,7 @@
+nama = "Dhafa Sahputra Jaelani"
+kelas = "TKJ X A"
+hobi = "badminton"
+
+print("Perkenalkan nama saya",nama)
+print("Saya kelas {kelas}")
+print("hobi saya {hobi},saya sangat senang melakukannya")
